568116922b448240069e890ead9b73d23fbd68b8
Administrator <langdawang678@sina.com>
2020-08-21 07:40:13 +0800

    repush

--- middleware/helper.py
+++ middleware/helper.py
@@ -5,6 +5,7 @@
 
 from common.requests_handler import RequestsHandler
 from config.setting import config
+from middleware.yaml_handler import yaml_data
 
 
 def login():
@@ -12,4 +13,16 @@ def login():
     # 数据方案1：从login的excel中获取
     # 数据方案2：从配置文件中获取
     """
-    res = RequestsHandler().visit(config.host )
+    res = RequestsHandler().visit(config.host+'/member/login',
+                                  'post',
+                                  json=yaml_data['user'],
+                                  headers={"X-Lemonban-Media-Type": "lemonban.v2"}
+                                  )
+    return res
+
+def save_token():
+    pass
+
+if __name__ == '__main__':
+    print(login())
+
--- middleware/yaml_handler.py
+++ middleware/yaml_handler.py
@@ -2,11 +2,22 @@
 # -*- coding: UTF-8 -*-
 # @date: 2020/8/20 8:53 下午
 # @author：langdawang678
+import yaml
+
+from config.setting import config
 
 
 class YamlHandler:
     def __init__(self, file):
-        pass
+        self.file = file
 
-    def read(self):
-        f = open(self.file)
+    def read(self, encoding='utf-8'):
+        f = open(self.file, encoding=encoding)
+        data = yaml.load(f, yaml.FullLoader)
+        f.close()
+        return data
+
+
+# 读取本项目的yaml配置
+yaml_data = YamlHandler(config.yaml_config_path).read()
+print(yaml_data)
