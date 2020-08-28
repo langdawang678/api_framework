64af9e2b4baa586f1d4be82e8e0ba6a74e412075
chenxuanhuai <langdawang678@sina.com>
2020-08-28 20:01:02 +0800

    repush-mac

--- middleware/helper.py
+++ middleware/helper.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*-
 # @date: 2020/8/20 8:45 下午
 # @author：langdawang678
+import re
 
 from common.requests_handler import RequestsHandler
 from config.setting import config
@@ -21,6 +22,12 @@ def login():
     return res
 
 
+def loan_id():
+    """
+    查询数据库，得到loan_id，临时变量保存到Context中
+    """
+    pass
+
 class Context:
     # 上下文/语境，存放临时数据。
     '''
@@ -29,14 +36,16 @@ class Context:
     方式1：setup中登录
     方式2：excel中，把登录case放在最前面
     '''
-    token = ''
-    member_id = None
+    member_id = 888
+    loan_id = 777
+    token = 'aofowelfsf'
+    username = 'yuz'
 
-def loan_id():
-    """
-    查询数据库，得到loan_id，临时变量保存到Context中
-    """
-    pass
+    @property
+    def loan_id(self):
+        """查询数据库，得到loan_id，临时便便存到Context中
+        return到loadn标当中的id值"""
+        pass
 
 def save_token():
     """保存token信息"""
@@ -58,7 +67,20 @@ def save_token():
     return {'token': token, 'memner_id': member_id}
 
 
+def replace_label(target):
+    re_pattern = r'#(.*?)#'
+    while re.findall(re_pattern, target):
+        # 如果能匹配
+        key = re.search(re_pattern, target).group(1)
+
+        target = re.sub(re_pattern, str(getattr(Context, key)), target, 1)
+    return target  # 注意return的层级，和while一起
+
+
 if __name__ == '__main__':
-    print(login())
-    data = save_token()
-    print(data)
+    # print(login())
+    # data = save_token()
+    # print(data)
+    mystr = '{"member_id":"#member_id#","loan_id":"#loan_id#","token":"#token#","username":"#username#"}'
+    a = replace_label(mystr)
+    print(a)
